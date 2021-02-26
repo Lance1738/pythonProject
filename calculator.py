2bd9f6ac76319dc6ebed7f407190036d93324eb9
Lance1738 <71152123+Lance1738@users.noreply.github.com>
2021-02-26 12:52:54 -0800

    Updated Code.

--- calculator.py
+++ calculator.py
@@ -2,19 +2,17 @@ num1 = float(input("Enter the first number: "))
 operator = input("Enter your operator here: ")
 num2 = float(input("Enter your second number here: "))
 
+
 if operator == "+":
     print(num1 + num2)
 
 if operator == "-":
     print(num1 - num2)
-
 if operator == "*":
     print(num1 * num2)
-
 if operator == "/":
     print(num1 / num2)
-
-if operator != "+, -, *, /":
+if operator not in ("+", "-", "*", "/"):
     print("Invalid Operator.")
 
 
